66714ead42956aac32431d99cb2cb06dd6b37063
bethyd <bethyd123@gmail.com>
2017-11-06 19:25:49 -0500

    adding models

--- detect_anime_face.py
+++ detect_anime_face.py
@@ -5,10 +5,9 @@ import numpy as np
 face_cascade = cv2.CascadeClassifier('lbpcascade_animeface.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-# def detect_anine_face(file):
-img = cv2.imread('SailorMoon2.jpg')
+def detect_anine_face(image):
+    img = cv2.imread(image)
 
-while True:
     # convert image to grayscale for easier processing
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -17,15 +16,5 @@ while True:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_color)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-    
-    # press 'ESC' to release camera
-    cv2.imshow('img', img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
 
-cap.release()
-cv2.destroyAllWindows()
-
+    return faces
